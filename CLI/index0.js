21a07f1752addde083592b1932252ee1d726eceb
안상근 <126879353+Ansuanggun@users.noreply.github.com>
2023-06-15 20:19:37 +0900

    asd
    aaa

---
+++ CLI/index0.js
@@ -0,0 +1,35 @@
+#!/usr/bin/env node
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const rand_0_9 = Math.floor(Math.random() * 10);
+console.clear();
+console.log("정답 미리 보기 : ",rand_0_9);
+
+const answerCallback = (answer) => {
+while(true){
+  if (answer === String(rand_0_9)) {
+    console.log('정답입니다. !');
+    rl.close();
+  } else if (answer < String(rand_0_9)) {
+    console.log('랜덤 수 보다 작습니다.');
+    rl.close();
+  } else if (answer > String(rand_0_9)) {
+    console.log('랜덤 수 보다 큽니다..');
+    rl.close();
+  } 
+  else {
+    console.clear();
+    console.log('0~9 숫자를 입력해주세요.');
+    rl.question('랜덤한 0~9 숫자를 입력하여 정답을 맞춰보세요.  ', answerCallback);
+  }
+}
+};
+
+rl.question('랜덤한 0~9 숫자를 입력하여 정답을 맞춰보세요.  ', answerCallback);
+
+
